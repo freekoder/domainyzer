ae791c213c84602a2645771a7be6f18e195c91d0
freekoder <freekoder@yandex.ru>
2022-03-31 17:10:03 +0300

    initial import

---
+++ src/main/java/com/vampbear/domainyzer/Domainyzer.java
@@ -0,0 +1,4 @@
+package com.vampbear.domainyzer;
+
+public class Domainyzer {
+}
---
+++ src/main/java/com/vampbear/domainyzer/Main.java
@@ -0,0 +1,7 @@
+package com.vampbear.domainyzer;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Domainyzer");
+    }
+}
