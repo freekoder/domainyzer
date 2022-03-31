57a8c52b7af83353bff32d633c5049576033c845
freekoder <freekoder@yandex.ru>
2022-03-31 19:46:06 +0300

    add count of founded domains

--- src/main/java/com/vampbear/domainyzer/Main.java
+++ src/main/java/com/vampbear/domainyzer/Main.java
@@ -4,11 +4,13 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+        String domain = "hackerone.com";
         System.out.println("Domainyzer");
         Domainyzer domainyzer = new Domainyzer();
-        List<String> subdomains = domainyzer.fromDomain("hackerone.com");
+        List<String> subdomains = domainyzer.fromDomain(domain);
         for (String subdomain : subdomains) {
             System.out.println(subdomain);
         }
+        System.out.printf("Found %d subdomains for %s%n", subdomains.size(), domain);
     }
 }
